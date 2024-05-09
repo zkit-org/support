5e23fa2cd7e6efd2fed7272c7dac0e4a03592219
Grant <284885069@qq.com>
2024-05-09 19:05:58 +0800

    feat: OTP服务

---
+++ cloud-starter/src/main/java/org/zkit/support/cloud/starter/otp/OTPConfiguration.java
@@ -0,0 +1,18 @@
+package org.zkit.support.cloud.starter.otp;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "auth.otp")
+@Data
+public class OTPConfiguration {
+
+    private String issuer;
+    private int secretSize = 32;
+    private String randomNumberAlgorithm = "SHA1PRNG";
+    private int windowSize = 1;
+    private int secondPerSize = 30;
+
+}
