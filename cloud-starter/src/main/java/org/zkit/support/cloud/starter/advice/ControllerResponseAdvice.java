09f08cd8b75c13feb449fede8f85c0872d4b61ce
Grant <284885069@qq.com>
2024-05-10 17:00:22 +0800

    feat: 表单验证

--- cloud-starter/src/main/java/org/zkit/support/cloud/starter/advice/ControllerResponseAdvice.java
+++ cloud-starter/src/main/java/org/zkit/support/cloud/starter/advice/ControllerResponseAdvice.java
@@ -4,16 +4,22 @@ import io.micrometer.common.lang.NonNullApi;
 import io.micrometer.common.lang.Nullable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import org.zkit.support.cloud.starter.entity.Result;
 import org.zkit.support.cloud.starter.exception.ResultException;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Slf4j
@@ -39,6 +45,20 @@ public class ControllerResponseAdvice implements ResponseBodyAdvice<Object> {
         return Result.error(e.getCode(), e.getMessage(), e.getData());
     }
 
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result<Object> validationHandler(MethodArgumentNotValidException e){
+        BindingResult bindingResult = e.getBindingResult();
+        if (!bindingResult.hasErrors()) {
+            return Result.error(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), null);
+        }
+        String res2 = bindingResult.getFieldErrors().stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        return Result.error(HttpStatus.BAD_REQUEST.value(), res2, null);
+    }
+
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public Result<Object> exceptionHandler(Exception e){
@@ -46,4 +66,5 @@ public class ControllerResponseAdvice implements ResponseBodyAdvice<Object> {
         e.printStackTrace();
         return Result.error("Server error");
     }
+
 }
