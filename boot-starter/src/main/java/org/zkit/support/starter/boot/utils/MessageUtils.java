4eb1969e9bb3fdcd158f0f118795d62aee9a0af9
Grant <284885069@qq.com>
2024-05-13 11:40:26 +0800

    feat: getLocale

--- boot-starter/src/main/java/org/zkit/support/starter/boot/utils/MessageUtils.java
+++ boot-starter/src/main/java/org/zkit/support/starter/boot/utils/MessageUtils.java
@@ -3,6 +3,8 @@ package org.zkit.support.starter.boot.utils;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import java.util.Locale;
+
 /**
  * 获取i18n资源文件
  * @author grant
@@ -18,5 +20,10 @@ public class MessageUtils {
     public static String get(String code, Object... args) {
         MessageSource messageSource = SpringUtils.getBean(MessageSource.class);
         return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+
+    public static String getLocale() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return locale.toString().replaceAll("_", "-");
     }
 }
