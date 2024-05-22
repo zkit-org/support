691a252ca467911b55397aa026362db01365c33c
Grant <284885069@qq.com>
2024-05-22 18:11:48 +0800

    feat: deploy to gitlab package

--- redisson-starter/src/main/java/org/zkit/support/starter/redisson/DistributedLock.java
+++ redisson-starter/src/main/java/org/zkit/support/starter/redisson/DistributedLock.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public @interface DistributedLock {
     String value() default ""; // 锁的名称
     String key() default ""; // 锁的key
-    long waitTime() default 30; // 获取锁等待时间，默认30秒
-    long leaseTime() default 60; // 锁自动释放时间，默认60秒
+    long waitTime() default 30L; // 等待时间，默认30秒
+    long leaseTime() default -1L; // -1表示永不过期
     TimeUnit timeUnit() default TimeUnit.SECONDS; // 时间单位，默认秒
 }
