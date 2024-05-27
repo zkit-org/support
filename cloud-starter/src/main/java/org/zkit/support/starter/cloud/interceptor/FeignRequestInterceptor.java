327efc55689bcdd5986eb27dd43975b524dc61e3
Grant <284885069@qq.com>
2024-05-27 11:34:47 +0800

    feat: 透传语言

--- cloud-starter/src/main/java/org/zkit/support/starter/cloud/interceptor/FeignRequestInterceptor.java
+++ cloud-starter/src/main/java/org/zkit/support/starter/cloud/interceptor/FeignRequestInterceptor.java
@@ -4,13 +4,22 @@ import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.jboss.logging.MDC;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.Locale;
 
 @Configuration
 public class FeignRequestInterceptor implements RequestInterceptor {
 
+    private static String getLocale() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return locale.toString().replaceAll("_", "-");
+    }
+
     @Override
     public void apply(RequestTemplate template) {
         template.header("x-trace-id", (String) MDC.get("traceId"));
+        template.header("Accept-Language", getLocale());
     }
 
 }
