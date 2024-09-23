bd047569d9e2d4cc3ed9a012ecae5c8db26b0781
Grant <284885069@qq.com>
2024-09-23 23:44:00 +0800

    feat: test log

--- skywalking-starter/src/main/java/org/zkit/support/starter/skywalking/TraceIdInterceptor.java
+++ skywalking-starter/src/main/java/org/zkit/support/starter/skywalking/TraceIdInterceptor.java
@@ -24,6 +24,7 @@ public class TraceIdInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView){
+        log.info("traceId===={}", TraceContext.traceId());
         response.setHeader("x-trace-id", TraceContext.traceId());
     }
 
