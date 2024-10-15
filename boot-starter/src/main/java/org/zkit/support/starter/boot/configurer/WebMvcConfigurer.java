5a59ba01427bcb0606bdc3d153148986abeb8375
Grant <284885069@qq.com>
2024-10-15 23:45:48 +0800

    feat: 安全性更新

--- boot-starter/src/main/java/org/zkit/support/starter/boot/configurer/WebMvcConfigurer.java
+++ boot-starter/src/main/java/org/zkit/support/starter/boot/configurer/WebMvcConfigurer.java
@@ -4,21 +4,11 @@ import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageC
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import java.util.List;
 
 @Configuration
 public class WebMvcConfigurer implements org.springframework.web.servlet.config.annotation.WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .maxAge(3600);
-    }
 
     @Override
     public void configureMessageConverters(@NonNull List<HttpMessageConverter<?>> converters) {
