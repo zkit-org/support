3c0901286e7042f443b8f47ca14ea2393cf9de74
Grant <284885069@qq.com>
2024-12-31 21:06:33 +0800

    feat: add RestTemplate

--- boot-starter/src/main/java/org/zkit/support/starter/boot/configurer/WebMvcConfigurer.java
+++ boot-starter/src/main/java/org/zkit/support/starter/boot/configurer/WebMvcConfigurer.java
@@ -1,9 +1,11 @@
 package org.zkit.support.starter.boot.configurer;
 
 import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.lang.NonNull;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -14,6 +16,11 @@ public class WebMvcConfigurer implements org.springframework.web.servlet.config.
     public void configureMessageConverters(@NonNull List<HttpMessageConverter<?>> converters) {
         org.springframework.web.servlet.config.annotation.WebMvcConfigurer.super.configureMessageConverters(converters);
         converters.add(0, new FastJsonHttpMessageConverter());
+    }
+
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
