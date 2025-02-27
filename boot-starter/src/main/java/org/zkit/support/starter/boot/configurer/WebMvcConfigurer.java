4581c2f626cc1f5409b70bf016d3be2512e59b47
Grant <284885069@qq.com>
2025-02-27 17:52:44 +0800

    feat: converters

--- boot-starter/src/main/java/org/zkit/support/starter/boot/configurer/WebMvcConfigurer.java
+++ boot-starter/src/main/java/org/zkit/support/starter/boot/configurer/WebMvcConfigurer.java
@@ -4,12 +4,9 @@ import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageC
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.lang.NonNull;
 import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Configuration
@@ -18,8 +15,7 @@ public class WebMvcConfigurer implements org.springframework.web.servlet.config.
     @Override
     public void configureMessageConverters(@NonNull List<HttpMessageConverter<?>> converters) {
         org.springframework.web.servlet.config.annotation.WebMvcConfigurer.super.configureMessageConverters(converters);
-        converters.add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
-        converters.add(1, new FastJsonHttpMessageConverter());
+        converters.add(0, new FastJsonHttpMessageConverter());
     }
 
     @Bean
