69a0ac23a3927fc504d12754929ce5f59606d1b5
Grant <284885069@qq.com>
2025-07-15 00:27:38 +0800

    feat: PageRequest add of method

--- mybatis-starter/src/main/java/org/zkit/support/starter/mybatis/entity/PageRequest.java
+++ mybatis-starter/src/main/java/org/zkit/support/starter/mybatis/entity/PageRequest.java
@@ -17,4 +17,11 @@ public class PageRequest implements Serializable {
         return PageHelper.startPage(page, size);
     }
 
+    public static PageRequest of(Integer page, Integer size) {
+        PageRequest pageRequest = new PageRequest();
+        pageRequest.setPage(page);
+        pageRequest.setSize(size);
+        return pageRequest;
+    }
+
 }
